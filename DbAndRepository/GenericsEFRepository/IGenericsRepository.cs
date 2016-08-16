c11b553347180c73faa7355cf4ca14861768773a
Benjámin Füzesi <eky0151@gmail.com>
2016-08-16 12:37:59 +0200

    New project for repository and database

---
+++ DbAndRepository/GenericsEFRepository/IGenericsRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace DbAndRepository.GenericsEFRepository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IGenericsRepository<TEntity> : IDisposable where TEntity : class
+    {
+        void Insert(TEntity newEntity);
+
+        void Delete(int id);
+
+        void Delete(TEntity entityToDelete);
+
+        void Update(TEntity entityToModify);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity GetById(int id);
+    }
+}
