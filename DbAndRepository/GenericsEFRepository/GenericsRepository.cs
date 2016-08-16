40a4978e3acd0bed3eeb2420a3ee967975e35f8c
Benjámin Füzesi <eky0151@gmail.com>
2016-08-16 12:49:45 +0200

    Addig IGenericsRepository and GenericsRepository

---
+++ DbAndRepository/GenericsEFRepository/GenericsRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAndRepository.GenericsEFRepository
+{
+    public abstract class GenericsRepository<TEntity> : IGenericsRepository<TEntity> where TEntity : class
+    {
+        protected internal DbContext database;
+
+        public GenericsRepository(DbContext newDb)
+        {
+            if (newDb == null)
+                throw new ArgumentNullException(nameof(newDb));
+
+            database = newDb;
+        }
+
+        public abstract void Delete(int id);
+        public abstract TEntity GetById(int id);
+        public abstract void Update(TEntity entityToModify);
+
+        public void Delete(TEntity entityToDelete)
+        {
+            database.Set<TEntity>().Remove(entityToDelete);
+            database.SaveChanges();
+        }
+
+        public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Where(predicate);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return database.Set<TEntity>();
+        }
+
+        public void Insert(TEntity newEntity)
+        {
+            database.Set<TEntity>().Add(newEntity);
+            database.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            database.Dispose();
+        }
+    }
+}
--- DbAndRepository/GenericsEFRepository/IGenericsRepository.cs
+++ DbAndRepository/GenericsEFRepository/IGenericsRepository.cs
@@ -1,11 +1,8 @@
 ﻿namespace DbAndRepository.GenericsEFRepository
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public interface IGenericsRepository<TEntity> : IDisposable where TEntity : class
     {
