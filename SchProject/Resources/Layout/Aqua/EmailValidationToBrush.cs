fa339660679897aed511c8ceee13169150d8b67a
Falus Dániel <dancs4545@gmail.com>
2016-08-19 21:55:28 +0200

    Add LoginWindow PreAlpha Version

---
+++ SchProject/Resources/Layout/Aqua/EmailValidationToBrush.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SchProject.Resources.Layout.Aqua
+{
+    class EmailValidationToBrush:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = value as string;
+            if (text != null)
+            {
+                if (
+                    Regex.IsMatch(text,
+                        @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+                {
+                    return (Color) ColorConverter.ConvertFromString("#95989A");
+                }
+                
+            }
+            return Brushes.Red;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
