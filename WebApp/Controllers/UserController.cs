092f00b1cebf8143686f678545d14f8946adcb76
hazibence1109 <hazibence1109@gmail.com>
2016-08-26 15:21:56 +0200

    new design without user page

---
+++ WebApp/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View("UserIndex");
+        }
+
+        public ActionResult Login()
+        {
+            //TODO: Check form input
+            Session["UserID"] = 42;
+            return RedirectToAction("Index");
+        }
+    }
+}
