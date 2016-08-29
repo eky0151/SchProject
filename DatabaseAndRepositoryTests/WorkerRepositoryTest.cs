4530bd64ec5b522155e66671806e631a38a99096
Benjámin Füzesi <eky0151@gmail.com>
2016-08-29 14:32:12 +0200

    Entites fixed

--- DatabaseAndRepositoryTests/WorkerRepositoryTest.cs
+++ DatabaseAndRepositoryTests/WorkerRepositoryTest.cs
@@ -9,49 +9,49 @@ namespace DatabaseAndRepositoryTests
     [TestClass]
     public sealed class WorkerRepositoryTest
     {
-        //private IWorkerRepository workerRepo;
-        //private DatabaseEntities db;
-        //private List<Worker> workerList;
+        private IWorkerRepository workerRepo;
+        private TechSupportDatabaseEntities db;
+        private List<Worker> workerList;
 
-        //#region Initialize and Cleanup
-        //[TestInitialize]
-        //public void InitializeTest()
-        //{
-        //    db = new DatabaseEntities();
-        //    workerRepo = new WorkerRepository(db);
-        //    workerList = new List<Worker>(workerRepo.GetAll());
-        //}
-        
-        //[TestCleanup]
-        //public void CleanUpTest()
-        //{
-        //    db.Dispose();
-        //    workerRepo.Dispose();
-        //    workerList = null;
-        //}
-        //#endregion
+        #region Initialize and Cleanup
+        [TestInitialize]
+        public void InitializeTest()
+        {
+           
+            workerRepo = new WorkerRepository(db);
+            workerList = new List<Worker>(workerRepo.GetAll());
+        }
 
-        //[TestMethod]
-        //public void ModifyWorkerData()
-        //{
-        //    Worker w = workerList[0];
-        //    string fullname = "John Doe";
-        //    w.Fullname = fullname;
-        //    workerRepo.Update(w);
-        //    workerList = new List<Worker>(workerRepo.GetAll());
-        //    Assert.AreEqual(fullname, workerList[0].Fullname);
-        //}
+        [TestCleanup]
+        public void CleanUpTest()
+        {
+            db.Dispose();
+            workerRepo.Dispose();
+            workerList = null;
+        }
+        #endregion
 
-        //[TestMethod]
-        //public void UpdateLogin()
-        //{
-        //    LoginDataRepository r = new LoginDataRepository(db);
-        //    List<LoginData> datas = new List<LoginData>(r.GetAll());
-        //    datas[0].Password = "new password";
+        [TestMethod]
+        public void ModifyWorkerData()
+        {
+            Worker w = workerList[0];
+            string fullname = "John Doe";
+            w.Fullname = fullname;
+            workerRepo.Update(w);
+            workerList = new List<Worker>(workerRepo.GetAll());
+            Assert.AreEqual(fullname, workerList[0].Fullname);
+        }
 
-        //    r.Update(datas[0]);
-        //}
+        [TestMethod]
+        public void UpdateLogin()
+        {
+            LoginDataRepository r = new LoginDataRepository(db);
+            List<LoginData> datas = new List<LoginData>(r.GetAll());
+            datas[0].Password = "new password";
 
-     
+            r.Update(datas[0]);
+        }
+
+
     }
 }
