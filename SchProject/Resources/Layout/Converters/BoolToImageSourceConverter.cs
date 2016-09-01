bbcdd145373b728c90051d0abeb3fb644b4e1d35
Falus Dániel <dancs4545@gmail.com>
2016-09-02 00:49:40 +0200

    Finish Technician control style

---
+++ SchProject/Resources/Layout/Converters/BoolToImageSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace SchProject.Resources.Layout.Converters
+{
+    public class BoolToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? selected = value as bool?;
+            if (selected.HasValue && selected.Value)
+            {
+                return new BitmapImage(new Uri(@"pack://application:,,,/Resources/Layout/Images/available.png"));
+            }
+            return new BitmapImage(new Uri(@"pack://application:,,,/Resources/Layout/Images/notavailable.png"));
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SchProject/ViewModel/AdminsViewModel.cs
+++ SchProject/ViewModel/AdminsViewModel.cs
@@ -4,12 +4,54 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using GalaSoft.MvvmLight;
+using Microsoft.Maps.MapControl.WPF;
 
 namespace SchProject.ViewModel
 {
+    public class AdminTemporaryModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public BitmapImage ProfilePicture { get; set; }
+        public bool Available { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AdditionalData { get; set; }
+        public Location LastLocation { get; set; }
+
+        public AdminTemporaryModel(int id, string name, BitmapImage profilePicture, bool avaible, string email, string phoneNumber, string additionalData, Location lastLocation)
+        {
+            ID = id;
+            Name = name;
+            ProfilePicture = profilePicture;
+            Available = avaible;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            AdditionalData = additionalData;
+            LastLocation = lastLocation;
+        }
+
+    }
     public class AdminsViewModel : ViewModelBase
     {
-        public ObservableCollection<string> Admins { get; set; }=new ObservableCollection<string>() {"soidfsdf","nisdofsmdof","nisdnfsd"};
+        private AdminTemporaryModel _selectedTechnician = new AdminTemporaryModel(1, "FSfadfas dfgsdfgs",
+            new BitmapImage(
+                new Uri(@"C:\Users\dancs\Documents\GitRepos\SchProject\SchProject\Resources\Layout\Images\demoPic.jpg")),
+            true, "momfsdfm@dsfd.gz", "0484848484", "Ugyféltől jön", new Location(47.539519, 19.074154));
+
+        public AdminTemporaryModel SelectedTechnician
+        {
+            get { return _selectedTechnician; }
+            set { Set(ref _selectedTechnician, value); }
+        }
+
+        public ObservableCollection<AdminTemporaryModel> Admins { get; set; } = new ObservableCollection<AdminTemporaryModel>()
+        {
+            new AdminTemporaryModel(1,"FSfadfas dfgsdfgs",new BitmapImage(new Uri(@"C:\Users\dancs\Documents\GitRepos\SchProject\SchProject\Resources\Layout\Images\demoPic.jpg")),true,"momfsdfm@dsfd.gz","0484848484","Ugyféltől jön",new Location(47.539519, 19.074154) ),
+            new AdminTemporaryModel(1,"ASafsd sdfsdaf",new BitmapImage(new Uri(@"C:\Users\dancs\Documents\GitRepos\SchProject\SchProject\Resources\Layout\Images\demoPic.jpg")),true,"fasdfasd@dsfd.gz","334563456","U56345gyféltől jön",new Location(47.589519, 19.074154) ),
+            new AdminTemporaryModel(1,"INMK jndsuiajnvs",new BitmapImage(new Uri(@"C:\Users\dancs\Documents\GitRepos\SchProject\SchProject\Resources\Layout\Images\demoPic.jpg")),false,"gasdg@dsfd.gz","345634","Ugyféltől jön",new Location(47.519519, 19.074154) )
+        };
     }
 }
