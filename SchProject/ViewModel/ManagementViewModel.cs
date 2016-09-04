c95837781aa17674dc653a411a93ce989de062fd
Falus Dániel <dancs4545@gmail.com>
2016-09-05 00:46:05 +0200

    Add SharedLibrary Add AzureBlobUploader Add UploadImage static method

--- SchProject/ViewModel/ManagementViewModel.cs
+++ SchProject/ViewModel/ManagementViewModel.cs
@@ -12,18 +12,19 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using SchProject.TechSupportService;
 using ImageProcessor;
+using TechSharedLibraries;
 
 namespace SchProject.ViewModel
 {
     public class ManagementViewModel : ViewModelBase
     {
-        public string FullName { get; set; } = "Harry Geza";
-        public string Username { get; set; } = "Maright";
-        public string Email { get; set; } = "MichaelADeal@dayrep.com";
-        public string Phone { get; set; } = "06304896357";
-        public string Address { get; set; } = "3152 Nemti Apáczai Csere János 16";
-        public string ProfilePicture { get; set; }
-        public string BankAccount { get; set; } = "5332773384";
+        private string _profilePicture;
+        public string FullName { get; set; }
+        public string Username { get; set; } 
+        public string Email { get; set; } 
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string BankAccount { get; set; } 
         public ICommand BrowseCommand { get; private set; }
         public ICommand SaveCommand { get; private set; }
 
@@ -32,12 +33,17 @@ namespace SchProject.ViewModel
             BrowseCommand = new RelayCommand(Browse);
             SaveCommand = new RelayCommand<PasswordBox>(Save);
         }
+        public string ProfilePicture
+        {
+            get { return _profilePicture; }
+            set { Set(ref _profilePicture, value); }
+        }
 
         private void Save(PasswordBox obj)
         {
             string uploaded = null;
             if (ProfilePicture != null)
-                uploaded = UploadImage();
+                uploaded = AzureBlobUploader.UploadImage(ProfilePicture);
             using (TechSupportService1Client client = new TechSupportService1Client())
             {
                 client.Open();
@@ -57,42 +63,5 @@ namespace SchProject.ViewModel
                 ProfilePicture = dlg.FileName;
             }
         }
-
-        private string UploadImage()
-        {
-            string fileName = Guid.NewGuid().ToString("N").Substring(0, 12);
-            ImageFactory factory = new ImageFactory();
-            factory.Load(ProfilePicture);
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer container = blobClient.GetContainerReference("images");
-            CloudBlockBlob originalBlob = container.GetBlockBlobReference($"original/{fileName}.png");
-            using (var fileStream = System.IO.File.OpenRead(ProfilePicture))
-            {
-                originalBlob.UploadFromStream(fileStream);
-            }
-            CloudBlockBlob mediumBlob = container.GetBlockBlobReference($"512/{fileName}.png");
-            using (var fileStream = new MemoryStream())
-            {
-                factory.Resize(new Size(512, 512)).Save(fileStream);
-                mediumBlob.UploadFromStream(fileStream);
-            }
-            CloudBlockBlob smallBlob = container.GetBlockBlobReference($"64/{fileName}.png");
-            using (var fileStream = new MemoryStream())
-            {
-                factory.Resize(new Size(64, 64)).Save(fileStream);
-                smallBlob.UploadFromStream(fileStream);
-            }
-            CloudBlockBlob extraSmallBlob = container.GetBlockBlobReference($"32/{fileName}.png");
-            using (var fileStream = new MemoryStream())
-            {
-                factory.Resize(new Size(32, 32)).Save(fileStream);
-                extraSmallBlob.UploadFromStream(fileStream);
-            }
-            return fileName + ".png";
-        }
-
-
-
     }
 }
