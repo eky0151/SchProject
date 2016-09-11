48b9fcb32797af6ff0500ab1190720d003879ee6
hazibence1109 <hazibence1109@gmail.com>
2016-09-11 12:53:12 +0200

    asp upload image prepare

--- WebApp/Controllers/RegisterController.cs
+++ WebApp/Controllers/RegisterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechSharedLibraries;
 using WebApp.Models;
 using WebApp.TechSupportServiceReference;
 
@@ -23,9 +24,34 @@ namespace WebApp.Controllers
             {
                 client.Open();
                 client.RegisterNewUser(fullname, email, username, password);
+                
             }
 
             return View(new RegisterModel(username, fullname, password, email));
         }
+
+        [HttpPost]
+        public ActionResult Registration(string username, string fullname, string password, string email, HttpPostedFileBase file)
+        {
+            using (TechSupportService1Client client = new TechSupportService1Client())
+            {
+                client.Open();
+                client.RegisterNewUser(fullname, email, username, password);
+
+                if (file != null)
+                {
+                    Upload(file);
+                }
+            }
+
+            return View(new RegisterModel(username, fullname, password, email));
+        }
+
+        private async void Upload(HttpPostedFileBase file)
+        {
+            //string path = System.IO.Path.Combine(Server.MapPath("~/images/profile"), pic);
+            //await AzureBlobUploader.UploadImageAsync(path);
+        }
+        
     }
 }
